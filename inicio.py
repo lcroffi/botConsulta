5eba341a32ff4ea0c90cac7e13c9804169e356bd
lcroffi <lileekat@hotmail.com>
2018-03-15 19:10:42 -0300

    Mudando a mensagem

--- inicio.py
+++ inicio.py
@@ -2,5 +2,5 @@ import telebot
 bot = telebot.TeleBot("503578488:AAEmi4K0-arlUIGXzg6WEsnCbaGQiReR-oQ")
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
-    bot.reply_to(message, u"Olá, eu sou a Lucy!")
+    bot.reply_to(message, u"Bom dia, eu sou a secretária Lucy. No que posso ajudá-lo?")
 bot.polling()
