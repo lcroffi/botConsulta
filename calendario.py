98f464b4df548f20667cd4190c73c4e1e255b99a
lcroffi <lileekat@hotmail.com>
2018-05-03 18:29:36 -0300

    Atualização da Larissa

---
+++ calendario.py
@@ -0,0 +1,184 @@
+from __future__ import print_function
+from apiclient.discovery import build
+from httplib2 import Http
+from oauth2client import file, client, tools
+import datetime
+
+SCOPES = 'https://www.googleapis.com/auth/calendar.readonly'
+store = file.Storage('credentials.json')
+creds = store.get()
+
+if not creds or creds.invalid:
+    flow = client.flow_from_clientsecrets('calendar.json', SCOPES)
+    creds = tools.run_flow(flow, store)
+service = build('calendar', 'v3', http=creds.authorize(Http()))
+
+now = datetime.datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
+print('Getting the upcoming 10 events')
+events_result = service.events().list(calendarId='primary', timeMin=now,
+                                      maxResults=10, singleEvents=True,
+                                      orderBy='startTime').execute()
+events = events_result.get('items', [])
+
+if not events:
+    print('No upcoming events found.')
+for event in events:
+    start = event['start'].get('dateTime', event['start'].get('date'))
+    print(start, event['summary'])
+
+
+# Refer to the Python quickstart on how to setup the environment:
+# https://developers.google.com/calendar/quickstart/python
+# Change the scope to 'https://www.googleapis.com/auth/calendar' and delete any
+# stored credentials.
+
+criando = {
+  "kind": 'calendar#event',
+  "etag": 'H9ZGKE3A7ycpPEdEBmqcA7jC',
+  "id": '107559896956929114877',
+  "status": "confirmed",
+  "htmlLink": '',
+  "created": '',
+  "updated": '',
+  "summary": 'teste 01',
+  "description": 'destando o código',
+  "colorId": string,
+  "creator": {
+    "id": string,
+    "email": string,
+    "displayName": string,
+    "self": boolean
+  },
+  "organizer": {
+    "id": string,
+    "email": string,
+    "displayName": string,
+    "self": boolean
+  },
+  "start": {
+    "date": date,
+    "dateTime": datetime,
+    "timeZone": string
+  },
+  "end": {
+    "date": date,
+    "dateTime": datetime,
+    "timeZone": string
+  },
+  "endTimeUnspecified": boolean,
+  "recurrence": [
+    string
+  ],
+  "recurringEventId": string,
+  "originalStartTime": {
+    "date": date,
+    "dateTime": datetime,
+    "timeZone": string
+  },
+  "transparency": string,
+  "visibility": string,
+  "iCalUID": string,
+  "sequence": integer,
+  "attendees": [
+    {
+      "id": string,
+      "email": string,
+      "displayName": string,
+      "organizer": boolean,
+      "self": boolean,
+      "resource": boolean,
+      "optional": boolean,
+      "responseStatus": string,
+      "comment": string,
+      "additionalGuests": integer
+    }
+  ],
+  "attendeesOmitted": boolean,
+  "extendedProperties": {
+    "private": {
+      (key): string
+    },
+    "shared": {
+      (key): string
+    }
+  },
+  "hangoutLink": string,
+  "conferenceData": {
+    "createRequest": {
+      "requestId": string,
+      "conferenceSolutionKey": {
+        "type": string
+      },
+      "status": {
+        "statusCode": string
+      }
+    },
+    "entryPoints": [
+      {
+        "entryPointType": string,
+        "uri": string,
+        "label": string,
+        "pin": string,
+        "accessCode": string,
+        "meetingCode": string,
+        "passcode": string,
+        "password": string
+      }
+    ],
+    "conferenceSolution": {
+      "key": {
+        "type": string
+      },
+      "name": string,
+      "iconUri": string
+    },
+    "conferenceId": string,
+    "signature": string,
+    "notes": string
+  },
+  "gadget": {
+    "type": string,
+    "title": string,
+    "link": string,
+    "iconLink": string,
+    "width": integer,
+    "height": integer,
+    "display": string,
+    "preferences": {
+      (key): string
+    }
+  },
+  "anyoneCanAddSelf": boolean,
+  "guestsCanInviteOthers": boolean,
+  "guestsCanModify": boolean,
+  "guestsCanSeeOtherGuests": boolean,
+  "privateCopy": boolean,
+  "locked": boolean,
+  "reminders": {
+    "useDefault": boolean,
+    "overrides": [
+      {
+        "method": string,
+        "minutes": integer
+      }
+    ]
+  },
+  "source": {
+    "url": string,
+    "title": string
+  },
+  "attachments": [
+    {
+      "fileUrl": string,
+      "title": string,
+      "mimeType": string,
+      "iconLink": string,
+      "fileId": string
+    }
+  ]
+}
+            
+    
+        
+evento = service.events().insert(calendarId='H9ZGKE3A7ycpPEdEBmqcA7jC', body=criando).execute()
+print ('Event created: %s' % (evento.get('https://calendar.google.com/calendar/r/agenda?pli=1')))
---
+++ telegram3.py
@@ -0,0 +1,144 @@
+import telepot
+import json
+from telepot.namedtuple import ForceReply, InlineKeyboardMarkup, InlineKeyboardButton, ReplyKeyboardMarkup, KeyboardButton
+
+
+####### Salvar e carregar memória do bot
+
+def memoriaBot():
+    try:
+        memoria = open('Lucy.json','r')
+    except FileNotFoundError:
+        memoria = open('Lucy.json','w')
+        memoria.write('[["Larissa","Letícia", "Jéssica"], {"oi": "Olá, com quem estou falando?","tchau":"Tchau!", "sair":"Até logo!"}, ["Médicos"]]')
+        memoria.close()
+        memoria = open('Lucy.json','r')
+    conhecidos, frases, medicos = json.load(memoria)
+    memoria.close()
+    historico = [None, ]
+
+####### Ações de interação do bot para deixá-lo mais humano
+
+def escuta(frase=None):
+        frase = str(frase)
+        frase = frase.lower()
+        return frase
+
+
+def pensa(frase):
+    if frase in frases:
+        return frases[frase]
+    if frase == 'aprende':
+            return 'Digite a frase: '
+    if frase == 'cardiologista' or frase == 'clínico geral' or frase == 'dermatologista' or frase == 'endocrinologista' or frase == 'gastroenterologista' or frase == 'ginecologista' or frase == 'oftalmologista' or frase == 'pneumologista':
+        return 'Temos um horário às 16h com a Dra. Cláudia, pode ser?'
+    if frase == 'outro':
+        return 'Qual seria a especialidade que está procurando?'
+    if frase == "/start":
+        return 'Bom dia, eu sou a secretária Lucy. No que posso ajudá-lo?'
+            
+    #Responde frases que dependem do histórico
+    ultimaFrase = historico[-1]
+    if ultimaFrase == 'Olá, com quem estou falando?':
+        nome = pegaNome(frase)
+        frase = respondeNome(nome)
+        return frase
+    if ultimaFrase == 'Digite a frase: ':
+        chave = frase
+        return 'Digite a resposta: '
+    if ultimaFrase == 'Digite a resposta: ':
+        resp = frase
+        frases[chave] = resp
+        gravaMemoria()
+        return 'Aprendido'
+    if ultimaFrase == 'Qual seria a especialidade que está procurando?':
+        medico = frase
+        medicos.append(medico)
+        gravaMemoria() 
+        return 'Anotado, vamos procurar essa especialidade.'
+            
+    try:
+        resp = str(eval(frase))
+        return resp
+    except:
+        pass
+    return 'Posso marcar uma consulta pra você.'
+
+            
+def pegaNome(nome):
+    nome = nome.title()
+    return nome
+
+def respondeNome(nome):
+        if nome in conhecidos:
+            frase = 'Olá outra vez, '
+        else:
+            frase = 'Muito prazer, '
+            conhecidos.append(nome)
+            gravaMemoria()            
+        return frase+nome
+    
+def gravaMemoria():
+    memoria = open('Lucy.json','w')
+    json.dump([conhecidos, frases, medicos],memoria)
+    memoria.close()
+        
+def fala(frase):
+    print(frase)
+    historico.append(frase)
+
+
+####### Rodar o bot no Telegram
+
+def build_menu(buttons,
+               n_cols,
+               header_buttons=None,
+               footer_buttons=None):
+    menu = [buttons[i:i + n_cols] for i in range(0, len(buttons), n_cols)]
+    if header_buttons:
+        menu.insert(0, header_buttons)
+    if footer_buttons:
+        menu.append(footer_buttons)
+    return menu
+
+def recebendoMsg(msg):
+    frase = escuta(frase=msg['text'])
+    resp = pensa(frase)
+    fala(resp)
+    #chatID = msg['chat']['id']
+    tipoMsg, tipoChat, chatID = telepot.glance(msg)
+    if 'consulta' in frase:
+            inline_keyboard = [
+                    InlineKeyboardButton(text="Cardiologista", callback_data='cardio'),
+                    InlineKeyboardButton(text="Clínico Geral", callback_data='clinic'),
+                    InlineKeyboardButton(text="Dermatologista", callback_data='dermo'),
+                    InlineKeyboardButton(text="Endocrinologista", callback_data='endoc'),
+                    InlineKeyboardButton(text="Gastroenterologista", callback_data='pneumo'),
+                    InlineKeyboardButton(text="Ginecologista", callback_data='gineco'),
+                    InlineKeyboardButton(text="Oftalmologista", callback_data='oftalmo'),
+                    InlineKeyboardButton(text="Pneumologista", callback_data='pneumo'),
+                    InlineKeyboardButton(text="Outro", callback_data='outro')
+            ]
+            reply_markup = InlineKeyboardMarkup(inline_keyboard=build_menu(button_list, n_cols=2))
+            medico = bot.sendMessage(chatID, 'Temos as seguintes especialidades disponíveis:', reply_markup=reply_markup)
+            
+    else:
+        bot.sendMessage(chatID,resp)
+
+def on_callback_query(msg):
+    query_id, from_id, query_data = telepot.glance(msg, flavor='callback_query')
+    chatID = from_id
+    medico = query_data
+    print(chatID)
+    bot.answerCallbackQuery(query_id, text="Só um instante")
+    print("callback query", query_id, from_id, query_data)
+
+memoriaBot()
+load = open("token.json")
+token = json.loads(load.read())
+bot = telepot.Bot(token)
+bot.message_loop({'chat': recebendoMsg,
+                  'callback_query': on_callback_query,})
+
+while True:
+    pass
